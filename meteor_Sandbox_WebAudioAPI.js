b6da725694cff958423dbaa75bce526bacf8f262
Addison Freeman <afree1423@gmail.com>
2016-03-24 18:07:49 -0500

    init repo, play thrust.wav

---
+++ meteor_Sandbox_WebAudioAPI.js
@@ -0,0 +1,26 @@
+if (Meteor.isClient) {
+  var context = new AudioContext();
+  //  oscillator = context.createOscillator();
+  //  oscillator.connect(context.destination);
+  //  oscillator.start(context.currentTime);
+  //  oscillator.stop(context.currentTime = 2);
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://localhost:3000/thrust.wav', true);
+  request.responseType = 'arraybuffer';
+  request.onload = function () {
+    var undecodedAudio = request.response;
+    context.decodeAudioData(undecodedAudio, function(buffer) {
+      var sourceBuffer = context.createBufferSource();
+      sourceBuffer.buffer = buffer;
+      sourceBuffer.connect(context.destination);
+      console.log(buffer);
+      sourceBuffer.start(context.currentTime);
+    });
+  };
+  request.send();
+
+}
+
+
+if (Meteor.isServer) {
+}
