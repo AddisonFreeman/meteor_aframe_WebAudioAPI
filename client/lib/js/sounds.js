b0cb96d6fc1c89855d2329d08bd03b5f13c6dde7
addisonfreeman <afree1423@gmail.com>
2016-06-07 21:39:30 -0500

    add scene from http://infiverse.com/code/linear-move/

--- client/lib/js/sounds.js
+++ client/lib/js/sounds.js
@@ -1,21 +1,39 @@
 Template.audio.onRendered(function () {
-    var context = new AudioContext();
-    //  oscillator = context.createOscillator();
-    //  oscillator.connect(context.destination);
-    //  oscillator.start(context.currentTime);
-    //  oscillator.stop(context.currentTime = 2);
-    var request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:3000/thrust.wav', true);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-      var undecodedAudio = request.response;
-      context.decodeAudioData(undecodedAudio, function(buffer) {
-        var sourceBuffer = context.createBufferSource();
-        sourceBuffer.buffer = buffer;
-        sourceBuffer.connect(context.destination);
-        console.log(buffer);
-        sourceBuffer.start(context.currentTime);
-      });
-    };
-    request.send();
-  })
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  var context = new AudioContext();
+  var sourceNode = context.createBufferSource();
+  var on = false;
+
+  $ = jQuery;
+  var box = $(".box");
+
+  console.log(box);
+  console.log(sourceNode);
+
+  var osc = context.createOscillator();
+  var real = new Float32Array([0,0.1,1,1,1,1,0.3,0.7,0.6,0.5,0,0.8]);
+  var imag = new Float32Array(real.length);
+  var wave = context.createPeriodicWave(real, imag);
+  osc.setPeriodicWave(wave);
+  osc.frequency.value = 60;
+  osc.connect(context.destination);
+  osc.start(context.currentTime);
+  $(document.body).click(function (e) {
+    osc.frequency.value = 600;
+  });
+
+
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://localhost:3000/thrust.wav', true);
+  request.responseType = 'arraybuffer';
+  request.onload = function () {
+    var undecodedAudio = request.response;
+    sourceNode.buffer = context.decodeAudioData(undecodedAudio).then(function(decodedData) {
+      sourceNode.buffer = decodedData;
+      sourceNode.connect(context.destination);
+      sourceNode.loop = true;
+      //sourceNode.start(context.currentTime);
+    });
+  };
+  request.send();
+  });
