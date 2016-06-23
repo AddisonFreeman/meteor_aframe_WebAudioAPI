c46b6adabd932aff5aa2c86878588215861afb89
addisonfreeman <afree1423@gmail.com>
2016-06-23 08:13:13 -0500

    create component (hrif and ambisonics)

--- client/meteor_aframe_WebAudioAPI.js
+++ client/meteor_aframe_WebAudioAPI.js
@@ -1,4 +1,82 @@
-$ = jQuery;
+var panner, cam, ice;
+
+AFRAME.registerComponent('audio', {
+	schema: {type: 'vec3' },
+	init: function() {
+	  // ----------------- Audio Things
+	  var AudioContext = window.AudioContext || window.webkitAudioContext;
+	  var audioContext = new AudioContext();
+	  // var playerBuffer = audioContext.createBuffer(2, 200, audioContext.sampleRate);
+	  var player = audioContext.createBufferSource();
+	  // player.buffer = playerBuffer;
+	
+	  // ---------------------------- local request file 
+	  var request = new XMLHttpRequest();
+	  request.open('GET', 'http://localhost:3000/04_Rhodes.wav', true);
+	  request.responseType = 'arraybuffer';
+	  request.onload = function () {
+	   var undecodedAudio = request.response;
+	   audioContext.decodeAudioData(undecodedAudio).then(function(decodedData) {
+	     player.buffer = decodedData;
+	     player.loop = true;
+	     //player.connect(audioContext.destination);
+	     player.start(0); 
+	   });
+	  };
+	  request.send();
+	
+	  //HRTF Panner
+	  hrtfContainer = new HRTFContainer();
+	  hrtfContainer.loadHrir("http://localhost:3000/kemar_L.bin");
+	  window.panner = new HRTFPanner(audioContext, player, hrtfContainer);
+	  window.panner.connect(audioContext.destination);  
+	  window.panner.update(90,0);
+	  cam = document.querySelector("#mainCamera");
+		console.log(panner);
+			  ice = document.querySelector("#iceberg");
+	    
+	},
+	tick: function() {
+		window.panner.update(90,10);
+	
+// 		window.panner.update( (Math.atan2(iceberg.getAttribute('position').z - cam.getAttribute('position').z,(iceberg.getAttribute('position').x - cam.getAttribute('position').x) ) * (180 / Math.PI) - 90),0);
+	    
+// 	    ice.setAttribute('position', (cam.getAttribute('position').x+10)+' 0 '+ cam.getAttribute('position').z+10);
+	}
+});
+
+
+AFRAME.registerComponent('FOA', {
+	schema: {type: 'vec3' },
+	init: function() {
+   	  var AudioContext = window.AudioContext || window.webkitAudioContext;
+	  var audioContext = new AudioContext();
+	  var player = audioContext.createBufferSource();
+	  // ---------------------------- local request file 
+	  var request = new XMLHttpRequest();
+	  request.open('GET', 'http://localhost:3000/04_Rhodes.wav', true);
+	  request.responseType = 'arraybuffer';
+	  request.onload = function () {
+	   var undecodedAudio = request.response;
+	   audioContext.decodeAudioData(undecodedAudio).then(function(decodedData) {
+	     player.buffer = decodedData;
+	     player.loop = true;
+	     //player.connect(audioContext.destination);
+	     player.start(0); 
+	   });
+	  };
+	  request.send();
+	      
+	      	    
+	},
+	tick: function() {
+		
+		
+		
+		
+	}
+});
+
 Template.peer.onCreated(function () {
 
   window.peer = new Peer({
@@ -17,12 +95,8 @@ Template.peer.onCreated(function () {
 });
 
 window.onload = function() {
-  var cam = document.querySelector("#mainCamera");
-  var iceberg = document.querySelector("#iceberg");
-
-  // function iceberg() {
-  //   iceberg.setAttribute('position', (cam.getAttribute('position').x + 10+' '+cam.getAttribute('position').y+' '+cam.getAttribute('position').z));
-  // }
+  cam = document.querySelector("#mainCamera");
+  iceberg = document.querySelector("#iceberg");
 
   peer.on('connection', function (conn) {
     console.log("you're connected!"+conn);
@@ -34,36 +108,31 @@ window.onload = function() {
   });
 
 
+  
+  //check if B above A on z-axis
+  // (iceberg.getAttribute('position').z - cam.getAttribute('position').z) > 0 
+
+  // && (iceberg.getAttribute('position').x - cam.getAttribute('position').x) > 0
 
 
-  // ----------------- Audio Things
-  var AudioContext = window.AudioContext || window.webkitAudioContext;
-  var audioContext = new AudioContext();
-  // var playerBuffer = audioContext.createBuffer(2, 200, audioContext.sampleRate);
-  var player = audioContext.createBufferSource();
-  // player.buffer = playerBuffer;
+/*
 
-  // ---------------------------- local request file 
-  var request = new XMLHttpRequest();
-  request.open('GET', 'http://localhost:3000/04_Rhodes.wav', true);
-  request.responseType = 'arraybuffer';
-  request.onload = function () {
-   var undecodedAudio = request.response;
-   audioContext.decodeAudioData(undecodedAudio).then(function(decodedData) {
-     player.buffer = decodedData;
-     player.loop = true;
-  //      player.connect(audioContext.destination);
-     // binauralPanner.connectInputByIndex(0, player);
-     player.start(0); 
-   });
-  };
-  request.send();
+  updPos = function () {
+    panner.update((Math.atan2(iceberg.getAttribute('position').z - cam.getAttribute('position').z,((iceberg.getAttribute('position').x - cam.getAttribute('position').x)))) * (180 / Math.PI) - 90,0);
+    console.log("updated sound");
+    setTimeout(updPos(), 1000); 
+  }
+*/
+  
 
-  //HRTF Panner
-  var hrtfContainer = new HRTFContainer();
-  hrtfContainer.loadHrir("./kemar_L.bin");
-  var panner = new HRTFPanner(audioContext, player, hrtfContainer);
-  panner.connect(audioContext.destination);  
+//   setTimeout(updPos(), 1000);
+
+  // function iceberg() {
+  //   iceberg.setAttribute('position', (cam.getAttribute('position').x + 10+' '+cam.getAttribute('position').y+' '+cam.getAttribute('position').z));
+  // }
+  
+  
+  
   // panner.update(0,0);
   // setTimeout(function () {
   //   panner.update(10,0);
